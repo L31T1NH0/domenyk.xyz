908d82c673198089b44aa30e12230a88a8c735d8
L31T1NH0 <oLeiteG@hotmail.com>
2022-12-29 23:47:47 -0300

    JS, CSS

--- script/app.js
+++ script/app.js
@@ -1,15 +1,9 @@
 const open_btn = document.querySelector('.open-popup');
 const close_btn = document.querySelector('.close-popup');
 const popup = document.querySelector('.popup');
-const main_popup = document.querySelector('.main-popup');
 open_btn.addEventListener('click', () => {
  popup.style.visibility = 'visible';
 });
 close_btn.addEventListener('click', () => {
     popup.style.visibility = 'hidden';
-});
-window.addEventListener('click', (e) => {
-    if(e.target == document.querySelector('.pix')){
-        popup.style.visibility = 'hidden';
-    }
 });
