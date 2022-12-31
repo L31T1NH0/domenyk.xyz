47c20460dcdf01b6c59f6ffb263b5587c1cede62
L31T1NH0 <oLeiteG@hotmail.com>
2022-12-31 00:26:53 -0300

    HTML / :hover / FormJS

---
+++ script/form.js
@@ -0,0 +1,15 @@
+const handleSubmit = (event) => {
+    event.preventDefault();
+    const nome = document.querySelector('textarea[name=nome]').value;
+    const mensagem = document.querySelector('textarea[name=msg]').value;
+    fetch('https://api.sheetmonkey.io/form/eJP734LDjojFFx6Ztu1g4N', {
+        method: 'post',
+        headers: {
+            'Accept': 'application/json',
+            'Content-type': 'application/json',
+        },
+        body: JSON.stringify({ nome, mensagem }),
+    }).then(function eraseText() {
+        document.getElementById("myForm").reset();
+    });}
+document.querySelector('form').addEventListener('submit', handleSubmit);
