ea54ae7abcb6ea05241baaccdd7625a2cd04c27f
L31T1NH0 <oLeiteG@hotmail.com>
2023-03-21 19:35:05 -0300

    Enter nice
    fixed: o uso do Enter no input
    fixex: o tamanho do input Name foi definido para 41 car's

--- Assets/JS/form.js
+++ Assets/JS/form.js
@@ -7,33 +7,46 @@ async function submitForm(event) {
   const mensagemInput = document.querySelector('textarea[name=msg]');
 
   // Obtendo os valores dos campos
-  const nome = nomeInput.value;
-  const mensagem = mensagemInput.value;
+  const nome = nomeInput.value.trim();
+  const mensagem = mensagemInput.value.trim();
 
-// Obtendo a data e hora atual formatada
-const date = new Date();
-const hora = date.toLocaleTimeString('pt-BR', {hour12: true, second: undefined});
-const dataHora = hora + ' ' + date.toLocaleDateString('pt-BR');
+  // Verificando se os campos estão preenchidos
+  if (mensagem === '') {
+    mensagemInput.focus();
+    return;
+  }
 
-// Enviando os dados do formulário para a API
-const response = await fetch('https://api.sheetmonkey.io/form/hexNZ9BHTtdqErQ1G573Av', {
-  method: 'post',
-  headers: {
-    'Accept': 'application/json',
-    'Content-type': 'application/json',
-  },
-  body: JSON.stringify({ Data: dataHora, Nome: nome, Mensagem: mensagem }),
-});
+  // Obtendo a data e hora atual formatada
+  const date = new Date();
+  const hora = date.toLocaleTimeString('pt-BR', {hour12: true, second: undefined});
+  const dataHora = hora + ' ' + date.toLocaleDateString('pt-BR');
+
+  // Enviando os dados do formulário para a API
+  const response = await fetch('https://api.sheetmonkey.io/form/hexNZ9BHTtdqErQ1G573Av', {
+    method: 'post',
+    headers: {
+      'Accept': 'application/json',
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify({ Data: dataHora, Nome: nome, Mensagem: mensagem }),
+  });
 
   // Verificando se a requisição foi bem-sucedida
   if (response.ok) {
     // Resetando os campos do formulário
     document.getElementById('myForm').reset();
-  } else {
-    console.error('Erro ao enviar formulário:', response.statusText);
   }
 }
 
 // Selecionando o formulário e adicionando listener de evento
 const form = document.querySelector('form');
 form.addEventListener('submit', submitForm);
+
+// Adicionando listener de evento para o input de mensagem
+const mensagemInput = document.querySelector('textarea[name=msg]');
+mensagemInput.addEventListener('keydown', function(event) {
+  if (event.keyCode === 13 && !event.shiftKey) {
+    event.preventDefault();
+    form.dispatchEvent(new Event('submit'));
+  }
+});
