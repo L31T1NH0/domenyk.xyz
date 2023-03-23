687c692f9ac69cf03da27e5a1fe54d7ee932ee5a
L31T1NH0 <oLeiteG@hotmail.com>
2023-03-23 01:27:13 -0300

    Update form.js

--- Assets/JS/form.js
+++ Assets/JS/form.js
@@ -1,9 +1,6 @@
 // Selecionando o campo de nome e adicionando ouvinte de evento de teclado
 const nomeInput = document.querySelector('textarea[name=nome]');
 nomeInput.addEventListener('keydown', function(event) {
-  if (event.keyCode === 13) {
-    event.preventDefault();
-  }
   if (event.keyCode === 13 && event.shiftKey) {
     event.preventDefault();
   }
@@ -16,15 +13,8 @@ async function getIp() {
   return data.ip;
 }
 
-// Função para obter a localização a partir das coordenadas geográficas
-async function getLocation(latitude, longitude) {
-  const response = await fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}&zoom=18&addressdetails=1&accept-language=pt-BR`);
-  const data = await response.json();
-  return data.address;
-}
-
 // Função para enviar o formulário
-async function submitForm(event, location) {
+async function submitForm(event) {
   event.preventDefault();
 
   // Selecionando os campos do formulário
@@ -41,9 +31,6 @@ async function submitForm(event, location) {
     return;
   }
 
-    // Obtendo o endereço IP do usuário
-    const ip = await getIp();
-
   // Resetando os campos do formulário
   event.target.reset();
 
@@ -55,6 +42,9 @@ async function submitForm(event, location) {
   // Obtendo informações do navegador e sistema operacional do usuário
   const platform = navigator.userAgentData.platform;
 
+  // Obtendo o endereço IP do usuário
+  const ip = await getIp();
+
   // Enviando os dados do formulário para a API
   await fetch('https://api.sheetmonkey.io/form/3w66mRcD3wLtQvf4fXDkXK', {
     method: 'post',
@@ -62,29 +52,13 @@ async function submitForm(event, location) {
       'Accept': 'application/json',
       'Content-type': 'application/json',
     },
-    body: JSON.stringify({País: location.country, Estado: location.state, Cidade: location.city, 'Endereço IP': ip, Sistema: platform, 'Data e Hora': dataHora, Nome: nome, Mensagem: mensagem}),
+    body: JSON.stringify({'Endereço IP': ip, Sistema: platform, Data: dataHora, Nome: nome, Mensagem: mensagem}),
   });
 }
 
 // Selecionando o formulário e adicionando listener de evento
 const form = document.querySelector('form');
 form.addEventListener('submit', submitForm);
-
-window.addEventListener('load', async function() {
-  // Obtendo a localização do usuário
-  const successCallback = async (position) => {
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
-    const location = await getLocation(latitude, longitude);
-
-    // Chamando a função submitForm com a localização obtida
-    const form = document.querySelector('form');
-    form.addEventListener('submit', (event) => submitForm(event, location));
-  };
-
-  // Obtendo as coordenadas geográficas do usuário
-  navigator.geolocation.getCurrentPosition(successCallback);
-});
 
 // Adicionando listener de evento para o input de mensagem
 const mensagemInput = document.querySelector('textarea[name=msg]');
@@ -93,4 +67,4 @@ mensagemInput.addEventListener('keydown', function(event) {
     event.preventDefault();
     form.dispatchEvent(new Event('submit'));
   }
-})
+});
