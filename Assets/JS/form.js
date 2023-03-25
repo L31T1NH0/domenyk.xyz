f7baa8ab5eb68ecb2ccf64fc3dad730afebc7d23
L31T1NH0 <oLeiteG@hotmail.com>
2023-03-25 00:36:14 -0300

    Update form.js

--- Assets/JS/form.js
+++ Assets/JS/form.js
@@ -1,11 +1,18 @@
-// Função assíncrona para obter o endereço IP do usuário
 async function getIp() {
   const response = await fetch('https://api.ipify.org/?format=json');
-  const { ip } = await response.json();
-  return ip;
+  const data = await response.json();
+  
+  // Adicionando alerta para verificar se a solicitação foi bem-sucedida ou não
+  if (response.ok) {
+    alert(`Endereço IP obtido com sucesso: ${data.ip}`);
+  } else {
+    alert(`Não foi possível obter o endereço IP. Código do erro: ${response.status}`);
+  }
+  
+  return data.ip;
 }
 
-// Função assíncrona para enviar o formulário
+// Função para enviar o formulário
 async function submitForm(event) {
   event.preventDefault();
 
@@ -18,7 +25,7 @@ async function submitForm(event) {
   const mensagem = mensagemInput.value.trim();
 
   // Verificando se os campos estão preenchidos
-  if (!mensagem) {
+  if (mensagem === '') {
     mensagemInput.focus();
     return;
   }
@@ -28,32 +35,37 @@ async function submitForm(event) {
 
   // Obtendo a data e hora atual formatada
   const date = new Date();
-  const hora = date.toLocaleTimeString('pt-BR', { hour12: true });
-  const dataHora = `${hora} ${date.toLocaleDateString('pt-BR')}`;
+  const hora = date.toLocaleTimeString('pt-BR', {hour12: true});
+  const dataHora = hora + ' ' + date.toLocaleDateString('pt-BR');
 
   // Obtendo informações do navegador e sistema operacional do usuário
   const platform = navigator.userAgentData.platform;
 
-  // Obtendo o endereço IP do usuário
-  const ip = await getIp();
+  try {
+    // Obtendo o endereço IP do usuário
+    const ip = await getIp();
 
-  // Enviando os dados do formulário para a API
-  await fetch('https://api.sheetmonkey.io/form/3w66mRcD3wLtQvf4fXDkXK', {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      'Endereço IP': ip,
-      Sistema: platform,
-      'Data e Hora': dataHora,
-      Nome: nome,
-      Mensagem: mensagem,
-    }),
-  });
+    // Enviando os dados do formulário para a API
+    const response = await fetch('https://api.sheetmonkey.io/form/3w66mRcD3wLtQvf4fXDkXK', {
+      method: 'post',
+      headers: {
+        'Accept': 'application/json',
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({'Endereço IP': ip, Sistema: platform, 'Data e Hora': dataHora, Nome: nome, Mensagem: mensagem}),
+    });
+
+    // Verificando se o envio foi bem-sucedido
+    if (response.ok) {
+      alert('Mensagem enviada com sucesso!');
+    } else {
+      throw new Error('Erro ao enviar a mensagem.');
+    }
+  } catch (error) {
+    alert('Ocorreu um erro ao enviar a mensagem:\n' + error.message);
+  }
 }
 
-// Selecionando o formulário e adicionando um listener de evento
+// Selecionando o formulário e adicionando listener de evento
 const form = document.querySelector('form');
 form.addEventListener('submit', submitForm);
